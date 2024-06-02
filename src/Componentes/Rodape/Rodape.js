f4f6afd0bb90bb123bc7d69ec77ff31c62ae5fd3
Victor Cardoso <victorclmatos@gmail.com>
2024-06-02 14:33:42 -0300

    ajuste do indice dos cards

--- src/Componentes/Rodape/Rodape.js
+++ src/Componentes/Rodape/Rodape.js
@@ -4,9 +4,7 @@ const Rodape = () => {
     return(
         <section className='rodape'>
             <div className='rodape-imgs-left'>
-                <img className='img-rodape' src='imgs/fb.png' alt='face-image'/>
-                <img className='img-rodape' src='imgs/tw.png' alt='twitter-image'/>
-                <img className='img-rodape' src='imgs/ig.png' alt='instagram-image'/>
+                <a href='https://www.linkedin.com/in/victor-cardoso-cloud-front/' target='a_blank'><img className='img-rodape' src='imgs/linkedin-icon.png' alt='linkedin-image'/></a>
             </div>
             <img className='rodape-imgs-center' src='imgs/logo.png' alt='logo-image'/>
             <h4>Desenvolvido por Victor Cardoso.</h4>
